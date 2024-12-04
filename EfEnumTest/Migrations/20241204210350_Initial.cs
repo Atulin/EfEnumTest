7f77f8efca6f4628953fd412342c3b53c29c75b9
Atulin <11233299+Atulin@users.noreply.github.com>
2024-12-04 22:04:40 +0100

    Initial commit

---
+++ EfEnumTest/Migrations/20241204210350_Initial.cs
@@ -0,0 +1,40 @@
+﻿using ;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace EfEnumTest.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:foo", "one,three,two");
+
+            migrationBuilder.CreateTable(
+                name: "Model",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Foo = table.Column<Foo>(type: "foo", nullable: false, defaultValue: Foo.One),
+                    Bar = table.Column<int>(type: "integer", nullable: false, defaultValue: 0)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Model", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Model");
+        }
+    }
+}
---
+++ EfEnumTest/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+Console.WriteLine("Hello, World!");
+
+return;
+
+internal sealed class Context : DbContext
+{
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		=> optionsBuilder
+			.EnableSensitiveDataLogging()
+			.LogTo(Console.WriteLine, LogLevel.Information)
+			.UseNpgsql(o => {
+				o.MapEnum<Foo>();
+			});
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<Model>(b => {
+			b.Property(p => p.Id)
+				.ValueGeneratedOnAdd();
+			b.Property(p => p.Foo)
+				.IsRequired()
+				.HasDefaultValue(Foo.One)
+				.HasSentinel((Foo)(-1));
+			b.Property(p => p.Bar)
+				.IsRequired()
+				.HasDefaultValue(Bar.Four)
+				.HasSentinel((Bar)(-1));
+		});
+	}
+}
+
+internal sealed class Model
+{
+	public int Id { get; set; }
+	public Foo Foo { get; set; }
+	public Bar Bar { get; set; }
+}
+
+internal enum Foo
+{
+	One,
+	Two,
+	Three
+}
+
+internal enum Bar
+{
+	Four,
+	Five,
+	Six
+}
